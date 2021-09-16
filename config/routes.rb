e393e1176ebb961651f857c42405117da0f6eb6c
david <dvdai63@gmail.com>
2021-09-16 22:37:47 +0700

    setup router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/feed', to: 'feed#index'
-  root to: "home#index"
+  get '/feeds', to: 'home#feeds'
+  get '/discover', to: 'home#discover'
+  root to: "home#feeds"
 end
