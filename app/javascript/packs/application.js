d166991931e291633f246447f4790d68d00b0ce6
david <dvdai63@gmail.com>
2021-09-20 23:46:39 +0700

    add boostrap model

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
+import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
