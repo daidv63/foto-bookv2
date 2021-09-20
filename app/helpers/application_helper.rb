a24bd01c02a03e4585936ad20986657615207903
david <dvdai63@gmail.com>
2021-09-20 23:28:40 +0700

    handle navigate photo/album

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 module ApplicationHelper
   require 'date'
+
   def formatDateTimePhoto()
     return DateTime.new(2007,11,19,8,37,48,"-06:00").strftime("%l:%M %P %d/%m/%G")
+  end
+
+  def checkPath(path)
+    return  request.original_fullpath.include?(path)
+  end
+
+  def checkParams(type)
+    unless request.query_parameters["type"]
+      return true
+    end
+    return request.query_parameters["type"] == type
   end
 end
